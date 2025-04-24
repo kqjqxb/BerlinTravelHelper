89f10c89fb2e2845416e72160e17d26cffc819bd
kqjqxb <work.maksym.lomakin@gmail.com>
2025-04-24 23:59:28 +0300

    some changes

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -7,30 +7,21 @@ export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    const loadHereUser = async () => {
+    const loadHereBerlinTravelUser = async () => {
       try {
-        const storedHereUser = await AsyncStorage.getItem('currentUser');
-        if (storedHereUser) {
-          setUser(JSON.parse(storedHereUser));
+        const storedHereBerlinTravelUser = await AsyncStorage.getItem('currentUser');
+        if (storedHereBerlinTravelUser) {
+          setUser(JSON.parse(storedHereBerlinTravelUser));
         }
       } catch (error) {
-        console.error('Error loading user data:', error);
+        console.error('Error loading berlin data of travel:', error);
       }
     };
-    loadHereUser();
+    loadHereBerlinTravelUser();
   }, []);
 
-  const logout = async () => {
-    try {
-      await AsyncStorage.removeItem('currentUser');
-      setUser(null);
-    } catch (error) {
-      console.error('Error logging out:', error);
-    }
-  };
-
   return (
-    <UserContext.Provider value={{ user, setUser, logout }}>
+    <UserContext.Provider value={{ user, setUser }}>
       {children}
     </UserContext.Provider>
   );
